9069facdc0d6e74589cee7d56cca484d820e7ee8
logangeorge03 <153015678+logangeorge03@users.noreply.github.com>
2024-11-12 23:59:57 -0700

    Update Medication.java

--- Medication.java
+++ Medication.java
@@ -5,4 +5,53 @@ public class Medication{
   private int quantity;
   private String description;
   private boolean restrictionStatus;
+
+  public void setType(String type){
+    this.type=type;
+  }
+
+  public String getType(){
+    return type;
+  }
+
+  public void setName(String name){
+    this.name=name;
+  }
+
+  public String getName(){
+    return name;
+  }
+
+  public void setCostPerGram(double cost){
+    this.costPerGram=cost;
+  }
+
+  public double getCostPerGram(){
+    return CostPerGram;
+  }
+
+  public void setQuantity(int quantity){
+    this.quantity=quantity;
+  }
+
+  public int getQuantity(){
+    return quantity;
+  }
+
+  public void setDescription(String description){
+    this.description=description;
+  }
+
+  public String getDescription(){
+    return description;
+  }
+
+  public void setStatus(boolean status){
+    this.restrictionStatus=status;
+  }
+
+  public boolean getStatus(){
+    return restrictionStatus;
+  }
+  
 }
