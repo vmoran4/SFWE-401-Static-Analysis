f41c2f106bab48d0a11039f17a9f00029e7b5906
Peyton Nagle <peytonnagle0@gmail.com>
2024-11-14 15:32:56 -0700

    Added getters and setters to Order.java

--- Order.java
+++ Order.java
@@ -6,6 +6,47 @@ public class Order{
     private int batchNumber;
     private String supplier;
 
+    // Getters and setters
+    public String getMedicationName() {
+        return medicationName;
+    }
+
+    public void setMedicationName(String medicationName) {
+        this.medicationName = medicationName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getExpDate() {
+        return expDate;
+    }
+
+    public void setExpDate(String expDate) {
+        this.expDate = expDate;
+    }
+
+    public int getBatchNumber() {
+        return batchNumber;
+    }
+
+    public void setBatchNumber(int batchNumber) {
+        this.batchNumber = batchNumber;
+    }
+
+    public String getSupplier() {
+        return supplier;
+    }
+
+    public void setSupplier(String supplier) {
+        this.supplier = supplier;
+    }
+
     public int getDaysTillExpiration(){
         // code to calculate days till expiration
         return 0;
